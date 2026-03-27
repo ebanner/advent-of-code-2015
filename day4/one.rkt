97939933fd8525a0bfb45701de5d99eb8121052c
Edward Banner <edward.banner@gmail.com>
2026-03-27 16:58:50 -0400

    ★

---
+++ day4/one.rkt
@@ -0,0 +1,34 @@
+#lang racket
+
+(provide get-hash-key)
+
+(require file/md5)
+
+(define ∞ (in-naturals))
+
+(define-syntax ~>
+  (syntax-rules ()
+    [(_ x) x]
+    [(_ x f rest ...) (~> (f x) rest ...)]))
+
+(define (valid? key)
+  (string-prefix? key "00000"))
+
+(define (my-hash secret-key i)
+  (let ([key (string-append secret-key (number->string i))])
+    (~>
+     key
+     string->bytes/utf-8
+     md5
+     bytes->string/utf-8)))
+
+(define/contract (get-hash-key secret-key)
+  (-> string? exact?)
+
+  (for/first ([i ∞]
+              #:when (valid? (my-hash secret-key i)))
+    i))
+
+(module+ main
+  (get-hash-key "iwrupvqb"))
+
