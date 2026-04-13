52a28c5730eddea55c5514f564740409d9385738
Edward Banner <edward.banner@gmail.com>
2026-04-13 19:22:58 -0400

    ★

---
+++ day6/one.clj
@@ -0,0 +1,82 @@
+(def GRID-SIZE 1000)
+
+(defmacro reduce* [[acc init x coll] & body]
+  `(reduce (fn [~acc ~x] ~@body) ~init ~coll))
+
+(defn get-action [line]
+  (let [tokens (clojure.string/split line #" ")]
+
+    (cond
+      (= (first tokens) "toggle") :TOGGLE
+      (= (subvec tokens 0 2) ["turn" "on"]) :TURN-ON
+      (= (subvec tokens 0 2) ["turn" "off"]) :TURN-OFF)))
+
+(defn parse-point [point-str]
+  (let [[i j] (clojure.string/split point-str #",")]
+    (mapv Integer/parseInt [i j])))
+
+(defn get-rectangle [line]
+  (let [tokens (clojure.string/split line #" ")
+        N (count tokens)]
+
+    [(parse-point (tokens (- N 3)))
+     (parse-point (last tokens))]))
+
+(defn parse [line]
+  (let [action (get-action line)
+        rectangle (get-rectangle line)]
+
+    [action rectangle]))
+
+(defn get-instructions []
+  (let [lines (clojure.string/split-lines (slurp "input"))]
+
+    (mapv parse lines)))
+
+(defn get-grid [N]
+  (->>
+   (for [_ (range N)] (into [] (repeat N 0)))
+   (into [])))
+
+(defn turn-on [grid [i1 j1] [i2 j2]]
+  (reduce* [grid grid
+            [i j] (for [i (range i1 (inc i2)) j (range j1 (inc j2))] [i j])]
+
+    (assoc-in grid [i j] 1)))
+
+(defn turn-off [grid [i1 j1] [i2 j2]]
+  (reduce* [grid grid
+            [i j] (for [i (range i1 (inc i2)) j (range j1 (inc j2))] [i j])]
+
+    (assoc-in grid [i j] 0)))
+
+(defn toggle [grid [i1 j1] [i2 j2]]
+  (reduce* [grid grid
+            [i j] (for [i (range i1 (inc i2)) j (range j1 (inc j2))] [i j])]
+
+    (update-in grid [i j] #(bit-xor % 1))))
+
+(defn do-instruction [[action [[i j] [ii jj]]] grid]
+  (cond
+    (= action :TURN-ON) (turn-on grid [i j] [ii jj])
+    (= action :TURN-OFF) (turn-off grid [i j] [ii jj])
+    (= action :TOGGLE) (toggle grid [i j] [ii jj])))
+
+(defn count-grid [grid]
+  (let [N (count grid)
+        M (count (first grid))]
+
+    (reduce +
+      (for [i (range N) j (range M)]
+        (get-in grid [i j])))))
+
+(defn -main [& args]
+  (let [instructions (get-instructions)
+        grid (-> (get-grid GRID-SIZE) atom)]
+
+    (doseq [instruction instructions]
+      (swap! grid #(do-instruction instruction %)))
+
+    (count-grid @grid)))
+
+(println (-main))
